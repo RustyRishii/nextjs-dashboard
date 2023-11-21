259334ffc6e7a2b050b49e47057af2c0a1b7dc4d
Rishikesh-278 <rishikeshbidkar04@gmail.com>
2023-11-21 11:12:30 +0530

    Initial commit

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import SideNav from '../ui/dashboard/sidenav';
+
+export default function Page() {
+  return (
+    <>
+      <p>This is a login Page</p>
+    </>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import {Inter} from "next/font/google";
+import { Teko } from "next/font/google";
+import { Lusitana } from "next/font/google";
+import { Noto_Serif } from "next/font/google";
+
+export const inter = Inter({subsets: ["latin"]});
+export const teko =  Teko({subsets: ["latin"]});
+export const lusitana = Lusitana({weight: ["400", "700"],subsets: ["latin"]})
+export const noto_Serif = Noto_Serif({subsets: ["latin"]})
